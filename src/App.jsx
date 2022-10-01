449b334b28c7547c54ddea62c58b86200bcd3908
Miguel Lopez <ing.miguellv@gmail.com>
2022-10-01 11:17:55 -0500

    add form to input presupuesto and validate it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 
+import Header from "./components/Header"
+
 function App() {
 
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
   return (
-    <h1>Control de gastos</h1>
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+    </div>
   )
 }
 
